45a1502b4c2c9cc110f3d1e6e0ebeb89a3afd32d
Foster McLane <fkmclane@gmail.com>
2016-12-11 15:25:43 -0500

    add runtime configuration

--- paste/config.py
+++ paste/config.py
@@ -5,6 +5,10 @@ addr = ('', 8080)
 log = '/var/log/paste/paste.log'
 httplog = '/var/log/paste/http.log'
 
+# template directory to use
+import os.path
+template = os.path.dirname(__file__) + '/html'
+
 # where service is located
 service = 'https://paste.fooster.io'
 
