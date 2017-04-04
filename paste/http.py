ef9763be5559830a305abd39a896743a2e26601c
Foster McLane <fkmclane@gmail.com>
2017-04-04 09:19:01 -0400

    add raw button and extensions to raw files

--- paste/http.py
+++ paste/http.py
@@ -1,3 +1,4 @@
+import mimetypes
 import time
 
 import web, web.form, web.page
@@ -76,10 +77,91 @@ class Paste(web.page.PageHandler):
         except KeyError:
             language_txt = 'Text'
 
-        return page.format(pygments=pygments.formatters.HtmlFormatter().get_style_defs('.highlight'), name=name, date=date, expire=expire, language=language_txt, code=highlighted)
+        return page.format(pygments=pygments.formatters.HtmlFormatter().get_style_defs('.highlight'), name=name, date=date, expire=expire, language=language_txt, code=highlighted, raw=self.request.resource + '/raw')
 
 
 class Raw(web.HTTPHandler):
+    extensions = {
+        # plain
+        'text/plain': '.txt',
+
+        # popular languages
+        'text/x-python': '.py',
+        'application/x-ruby': '.rb',
+        'text/x-gosrc': '.go',
+        'application/javascript': '.js',
+        'text/x-java': '.java',
+        'text/x-swift': '.swift',
+        'text/x-csharp': '.cs',
+        'text/x-csrc': '.c',
+        'text/x-c++src': '.cpp',
+
+        # programming languages
+        'application/x-actionscript': '.as',
+        'application/x-actionscript3': '.as',
+        'text/x-arduino': '.ino',
+        'text/coffeescript': '.coffee',
+        'text/x-dsrc': '.d',
+        'text/x-erlang': '.erl',
+        'text/x-fortran': '.f',
+        'text/x-glslsrc': '.glsl',
+        'text/x-haskell': '.hs',
+        'text/x-julia': '.jl',
+        'text/x-common-lisp': '.lisp',
+        'text/x-lua': '.lua',
+        'text/matlab': '.m',
+        'text/x-ocaml': '.ml',
+        'text/x-objective-c': '.m',
+        'application/x-php': '.php',
+        'text/rust': '.rs',
+        'text/x-scala': '.scala',
+        'text/x-scheme': '.scm',
+        'text/x-typescript': '.ts',
+        'text/x-vala': '.vala',
+
+        # low-level languages
+        'text/x-gas': '.s',
+        'text/x-nasm': '.asm',
+        'text/x-windows-registry': '.reg',
+        'text/x-vhdl': '.vhdl',
+        'text/x-verilog': '.v',
+
+        # markup languages
+        'text/html': '.html',
+        'text/css': '.css',
+        'text/x-gnuplot': '.gpi',
+        'application/json': '.json',
+        'text/x-less-css': '.less',
+        'application/postscript': '.ps',
+        'text/x-sass': '.sass',
+        'text/x-scss': '.scss',
+        'text/x-tex': '.tex',
+        'text/xml': '.xml',
+        'text/x-yaml': '.yaml',
+
+        # shell languages
+        'application/x-fish': '.fish',
+        'text/x-powershell': '.ps1',
+        'application/x-sh': '.sh',
+
+        # configuration languages
+        'text/x-apacheconf': '.conf',
+        'text/x-dockerfile-config': '.docker',
+        'text/x-ini': '.ini',
+        'text/x-nginx-conf': '.conf',
+        'text/x-java-properties': '.properties',
+        'text/x-vim': '.vim',
+
+        # code building languages
+        'text/x-cmake': '.cmake',
+        'text/x-makefile': '.makefile',
+
+        # other languages
+        'text/x-diff': '.patch',
+        'text/x-irclog': '.irc',
+        'text/x-sql': '.sql',
+    }
+
     def do_get(self):
         alias = self.groups[0]
 
@@ -87,6 +169,13 @@ class Raw(web.HTTPHandler):
             name, date, expire, language, code = paste.get(alias)
         except KeyError:
             raise web.HTTPError(404)
+
+        # add extension if necessary
+        if mimetypes.guess_type(name)[0] != language:
+            try:
+                name += self.extensions[language]
+            except KeyError:
+                pass
 
         # set headers
         self.response.headers['Content-Type'] = language
